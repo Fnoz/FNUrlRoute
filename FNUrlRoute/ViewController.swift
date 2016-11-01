1c22f928359ebaaf00c89f014d3e900392fe42eb
Fnoz <fnoz@sina.com>
2016-11-02 02:48:06 +0800

    Initial Commit.

---
+++ FNUrlRoute/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FNUrlRoute
+//
+//  Created by Fnoz on 2016/10/31.
+//  Copyright © 2016年 Fnoz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
